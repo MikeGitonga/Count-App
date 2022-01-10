ef90180a310a20b40c60e05880bb5031ce4becc9
MikeGitonga <93003806+MikeGitonga@users.noreply.github.com>
2022-01-10 06:57:22 +0300

    Add files via upload
    Counting app

---
+++ index.js
@@ -0,0 +1,27 @@
+let countEl= document.getElementById("count-el")
+console.log(countEl)
+
+
+let count = 0
+function increase(){
+    count += 1
+    countEl.textContent = count
+}
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+    
+    
+}
+
+let saveEl = document.getElementById("save-el")
+console.log(saveEl)
+
+
+
+
+
+
+
